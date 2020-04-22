62490807ca992b50757df5084127137d06b31a72
ryuyaF <r.fukushima403@gmail.com>
2020-04-23 02:37:40 +0900

    firebaseの接続情報をenvから取得するように修正

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,8 +1,12 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require("./serviceAccount");
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert({
+        "project_id": process.env.FIREBASE_PROJECT_ID,
+        "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+        "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    }),
+    databaseURL: "https://chat-sample-26721.firebaseio.com"
 });
 
 const db = admin.firestore();
